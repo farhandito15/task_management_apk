65470d603dfc88f71bac6e5d858cc0e9c1add188
farhandito15 <farhandito15@gmail.com>
2022-07-07 10:49:37 +0700

    commit home video 3

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,17 +5,137 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      key: _drawerKey,
+      drawer: const SideBar(),
+      backgroundColor: AppColors.primaryBg,
+      body: SafeArea(
+        child: Row(
+          children: [
+            !context.isPhone
+                ? const Expanded(
+                    flex: 2,
+                    child: SideBar(),
+                  )
+                : const SizedBox(),
+            Expanded(
+              flex: 16,
+              child: Column(children: [
+                !context.isPhone
+                    ? const header()
+                    : Container(
+                        padding: const EdgeInsets.all(20),
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                _drawerKey.currentState!.openDrawer();
+                              },
+                              icon: const Icon(
+                                Icons.menu,
+                                color: AppColors.primaryText,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            Column(
+                              //mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Task Management',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: AppColors.primaryText),
+                                ),
+                                Text(
+                                  'Manage task made easy with friends',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: AppColors.primaryText),
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            const Icon(
+                              Ionicons.notifications,
+                              color: AppColors.primaryText,
+                              size: 30,
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(30),
+                              child: const CircleAvatar(
+                                backgroundColor: Colors.amber,
+                                radius: 25,
+                                foregroundImage: NetworkImage(
+                                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                // content / isi page / screen
+                Expanded(
+                  child: Container(
+                    padding: !context.isPhone
+                        ? const EdgeInsets.all(40)
+                        : const EdgeInsets.all(20),
+                    margin: !context.isPhone
+                        ? const EdgeInsets.all(10)
+                        : const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: !context.isPhone
+                          ? BorderRadius.circular(40)
+                          : BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: Get.height * 0.4,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'My Task',
+                                  style: TextStyle(
+                                    color: AppColors.primaryText,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                // my task
+                                MyTask(),
+                              ],
+                            ),
+                          ),
+                          !context.isPhone
+                              ? Expanded(
+                                  child: Row(
+                                    children: const [
+                                      UpcomingTask(),
+                                      MyFriends(),
+                                    ],
+                                  ),
+                                )
+                              : const UpcomingTask(),
+                        ]),
+                  ),
+                )
+              ]),
+            ),
+          ],
         ),
       ),
     );
