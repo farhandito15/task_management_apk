eb5cde21375a5970bc91ae803fe879fcfb64a02e
farhandito15 <farhandito15@gmail.com>
2022-07-07 11:57:05 +0700

    commit transisi

--- lib/app/modules/task/views/task_view.dart
+++ lib/app/modules/task/views/task_view.dart
@@ -1,22 +1,104 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app2/app/utils/style/AppColors.dart';
+import 'package:task_management_app2/app/utils/widget/header.dart';
+import 'package:task_management_app2/app/utils/widget/sidebar.dart';
 
-import '../controllers/task_controller.dart';
+import '../controllers/friends_controller.dart';
 
 class TaskView extends GetView<TaskController> {
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TaskView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'TaskView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      key: _drawerKey,
+      drawer: const SideBar(),
+      backgroundColor: AppColors.primaryBg,
+      body: Row(
+        children: [
+          !context.isPhone
+              ? const Expanded(
+                  flex: 2,
+                  child: SideBar(),
+                )
+              : const SizedBox(),
+          Expanded(
+            flex: 15,
+            child: Column(children: [
+              !context.isPhone
+                  ? const header()
+                  : Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              _drawerKey.currentState!.openDrawer();
+                            },
+                            icon: const Icon(
+                              Icons.menu,
+                              color: AppColors.primaryText,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            //mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Text(
+                                'Task Management',
+                                style: TextStyle(
+                                    fontSize: 20, color: AppColors.primaryText),
+                              ),
+                              Text(
+                                'Manage task made easy with friends',
+                                style: TextStyle(
+                                    fontSize: 15, color: AppColors.primaryText),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Ionicons.notifications,
+                            color: AppColors.primaryText,
+                            size: 30,
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: const CircleAvatar(
+                              backgroundColor: Colors.amber,
+                              radius: 25,
+                              foregroundImage: NetworkImage(
+                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSibTMP3266859JrPVaoYmIXXinEtq9Bn_hfg&usqp=CAU'),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+              // content / isi page / screen
+              Expanded(
+                  child: Container(
+                padding: const EdgeInsets.all(50),
+                margin: !context.isPhone
+                    ? const EdgeInsets.all(10)
+                    : const EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: !context.isPhone
+                        ? BorderRadius.circular(50)
+                        : BorderRadius.circular(30)),
+              ))
+            ]),
+          ),
+        ],
       ),
     );
   }
