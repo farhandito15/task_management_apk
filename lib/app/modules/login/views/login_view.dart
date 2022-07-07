a29824ef89c8cace457f275c37956a05c09f216a
farhandito15 <farhandito15@gmail.com>
2022-07-07 09:44:08 +0700

    Initial commit

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_apk/app/routes/app_pages.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Container(
+        margin: context.isPhone
+            ? EdgeInsets.all(Get.width * 0.1)
+            : EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+        ),
+        child: Row(children: [
+          //biru
+          !context.isPhone
+              ? Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        bottomLeft: Radius.circular(50),
+                      ),
+                      color: Colors.pink,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text(
+                              'Youkoso',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 70,
+                              ),
+                            ),
+                            Text(
+                              'Silahkan Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                              ),
+                            ),
+                            Text(
+                              'Dan mulai Menjelajah bersama kami',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ]),
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+          //putih
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    context.isPhone
+                        ? Column(
+                            //crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                                Text(
+                                  'Welcome',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 40,
+                                  ),
+                                ),
+                                Text(
+                                  'Please Sign In',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  'Start Journey With Us',
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ])
+                        : const SizedBox(),
+                    Image.asset(
+                      'assets/images/login.png',
+                      height: Get.height * 0.5,
+                    ),
+                    FloatingActionButton.extended(
+                      onPressed: () => Get.toNamed(Routes.HOME),
+                      label: const Text('Sign In With Google'),
+                      icon:
+                          const Icon(Ionicons.logo_google, color: Colors.white),
+                    )
+                  ]),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
